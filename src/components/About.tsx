baacf87be8fc9a4e52bcf81f81770efd561b855f
Aziz-BenLamine <azizbenlamine@gmail.com>
2024-12-25 16:05:00 +0100

    Add tabs && content

---
+++ src/components/About.tsx
@@ -0,0 +1,20 @@
+function About() {
+  return (
+    <section className="bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-4xl mx-auto">
+        <h2 className="text-3xl font-extrabold text-gray-900 mb-6">About Me</h2>
+        <p className="tex-lg text-gray-700">
+          I am a dedicated software engineering student with a passion for
+          developing innovative programs that expedite the efficiency and
+          effectiveness of organizational success. Well-versed in technology and
+          writing code to create systems that are reliable and user-friendly.
+          Confident communicator, strategic thinker, and innovative creator to
+          develop software that is customized to meet a company’s organizational
+          needs, highlight their core competencies, and further their success.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
